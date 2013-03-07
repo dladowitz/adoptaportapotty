a1bd3662f4a57e5dc1900831e928774dd4511ed5
David Ladowitz <david@ladowitz.com>
2013-03-06 23:07:46 -0800

    created repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdoptAPortaPotty::Application.config.secret_token = '0fc3f678977f7f6ce7ec22221ce7fedc206672ad3b8de861dbf189bb81c7291c61a7908d3fc1c8a4cc0587e63be8635a2db37ce91a4b82d863fb9e15f1af90cf'
